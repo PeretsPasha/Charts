c5ada6870c6a520a1c455f089ec28cd5c064562e
PeretsPasha <perets.pasha.pro@gmail.com>
2020-04-27 15:10:18 +0300

    Initial commit

---
+++ app/src/main/java/com/example/charts/RadarChartActivity.java
@@ -0,0 +1,14 @@
+package com.example.charts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RadarChartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_radar_chart);
+    }
+}
