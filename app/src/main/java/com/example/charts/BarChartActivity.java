eb6ae3a936b4263698b8676dedd563db04e67559
PeretsPasha <perets.pasha.pro@gmail.com>
2020-04-27 18:46:29 +0300

    Add a new Bar Chart

--- app/src/main/java/com/example/charts/BarChartActivity.java
+++ app/src/main/java/com/example/charts/BarChartActivity.java
@@ -2,7 +2,16 @@ package com.example.charts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 
 public class BarChartActivity extends AppCompatActivity {
 
@@ -10,5 +19,29 @@ public class BarChartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bar_chart);
+
+        BarChart barChart = findViewById(R.id.barCart);
+
+        ArrayList<BarEntry> visitors = new ArrayList<>();
+        visitors.add(new BarEntry(2014, 420));
+        visitors.add(new BarEntry(2015, 520));
+        visitors.add(new BarEntry(2016, 380));
+        visitors.add(new BarEntry(2017, 730));
+        visitors.add(new BarEntry(2018, 670));
+        visitors.add(new BarEntry(2019, 490));
+        visitors.add(new BarEntry(2020, 320));
+
+        BarDataSet barDataSet = new BarDataSet(visitors, "Відвідувачі");
+        barDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+        barDataSet.setValueTextColor(Color.BLACK);
+        barDataSet.setValueTextSize(16f);
+
+        BarData barData = new BarData(barDataSet);
+
+        barChart.setFitBars(true);
+        barChart.setData(barData);
+        barChart.getDescription().setText("Графік відвідування");
+        barChart.animateY(2000);
+
     }
 }
