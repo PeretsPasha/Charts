31140b66e47bf7c176d159e7c21d43061428c35e
PeretsPasha <perets.pasha.pro@gmail.com>
2020-04-28 16:34:31 +0300

    The last Chart has been added

--- app/src/main/java/com/example/charts/RadarChartActivity.java
+++ app/src/main/java/com/example/charts/RadarChartActivity.java
@@ -2,7 +2,22 @@ package com.example.charts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.charts.RadarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.data.RadarData;
+import com.github.mikephil.charting.data.RadarDataSet;
+import com.github.mikephil.charting.data.RadarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 
 public class RadarChartActivity extends AppCompatActivity {
 
@@ -10,5 +25,30 @@ public class RadarChartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_radar_chart);
+
+        RadarChart radarChart = findViewById(R.id.radarCart);
+
+        ArrayList<RadarEntry> visitors = new ArrayList<>();
+        visitors.add(new RadarEntry(710));
+        visitors.add(new RadarEntry(600));
+        visitors.add(new RadarEntry(740));
+        visitors.add(new RadarEntry(700));
+        visitors.add(new RadarEntry(730));
+
+        RadarDataSet radarDataSet = new RadarDataSet(visitors, "Відвідувачі");
+        radarDataSet.setColor(Color.RED);
+        radarDataSet.setLineWidth(2f);
+        radarDataSet.setValueTextColor(Color.RED);
+        radarDataSet.setValueTextSize(14f);
+
+        RadarData radarData = new RadarData(radarDataSet);
+
+        String[] labels = {"2016", "2017", "2018", "2019", "2020"};
+
+        XAxis xAxis = radarChart.getXAxis();
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(labels));
+
+        radarChart.getDescription().setText("Приклад RadarChart");
+        radarChart.setData(radarData);
     }
 }
